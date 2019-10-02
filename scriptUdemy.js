72e38a0b401629d3eee48543ea4df8759ac975da
SoundM <galina45192@gmail.com>
2019-10-02 23:16:07 +0200

    First

--- scriptUdemy.js
+++ scriptUdemy.js
@@ -1,11 +1,21 @@
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
 
-var number = 5;
-var string = 'ku';
-console.log(string);
-console.log(number);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-console.log(string / 0);
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-alert("hello!");
-
+alert(appData.budget / 30);
